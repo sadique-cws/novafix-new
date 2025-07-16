a81f9c15075f4096bccfe9b9ff91e6ebbd829061
sadique-cws <sadique.hussain96@gmail.com>
2025-07-16 16:58:03 +0530

    feat: Add franchise login and dashboard components with routes

---
+++ app/Livewire/Franchise/Dashboard.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Livewire\Franchise;
+
+use Livewire\Component;
+
+class Dashboard extends Component
+{
+    public function render()
+    {
+        return view('livewire.franchise.dashboard');
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -4,6 +4,8 @@ use App\Livewire\Admin\AddFranchises;
 use App\Livewire\Admin\AdminDashboard;
 use App\Livewire\Admin\ManageFranchises;
 use App\Livewire\Admin\ViewFranchises;
+use App\Livewire\Franchise\Dashboard;
+use App\Livewire\Franchise\Login;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
@@ -15,3 +17,11 @@ Route::get('admin/add-franchise',AddFranchises::class)->name('admin.add-franchis
 Route::get('admin/manage-franchises',ManageFranchises::class)->name('admin.manage-franchises');
 Route::get('/admin/view-franchises/{id?}', ViewFranchises::class)
     ->name('admin.view-franchises');
+
+
+Route::prefix("franchise")->group(function(){
+    Route::get("/login", Login::class)->name("franchise.login");
+    // Route::middleware(['auth'])->group(function () {
+    Route::get("/dashboard", Dashboard::class)->name("franchise.dashboard");
+    // });
+});
