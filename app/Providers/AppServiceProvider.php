e8f138cd5cc0c426d4ccc0b1d7f20ac7bb65c20e
sadique-cws <sadique.hussain96@gmail.com>
2026-04-17 23:03:12 +0530

    feat: add dynamic public path configuration for shared hosting environments

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -15,6 +15,11 @@ class AppServiceProvider extends ServiceProvider
         $this->app->singleton(Msg91Service::class, function ($app) {
             return new Msg91Service();
         });
+
+        // Useful for Shared Hosting where public/ is often public_html/ or root
+        if (file_exists(base_path('../public_html'))) {
+            $this->app->usePublicPath(base_path('../public_html'));
+        }
     }
 
     /**
